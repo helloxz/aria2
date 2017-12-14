9cb4bee3cf52ac2573686a2dd3c16bb2309a938a
xiaoz <xiaoz93@outlook.com>
2017-12-14 10:27:49 +0800

    beta

--- install_aria2.sh
+++ install_aria2.sh
@@ -24,62 +24,62 @@ function centos6(){
 	rm -rf *.rpm
 }
 
-#来不及了，后面再适配Debian吧
+#debian or ubuntu安装
 function debian(){
 	apt-get -y install aria2
 }
 
 #创建目录与配置
-function setting(){
-	mkdir -p /data/aria2
-	mkdir -p /data/aria2/download
-	touch /data/aria2/aria2.session
-	cp aria2.conf caddy.conf aria2.sh /data/aria2
+#function setting(){
+#	mkdir -p /data/aria2
+#	mkdir -p /data/aria2/download
+#	touch /data/aria2/aria2.session
+#	cp aria2.conf caddy.conf aria2.sh /data/aria2
 	
-	echo "-------------------------------"
-	read -p "设置用户名：" user
-	read -p "设置密码：" pass
-	read -p "设置Aria2授权令牌（字母或数字组合，不要包含特殊字符）：" rpc
-	echo "-------------------------------"
-	sed -i "s/rpc-secret=/rpc-secret=${rpc}/g" /data/aria2/aria2.conf
-	#下载yaaw
-	wget -P /data/aria2 https://github.com/binux/yaaw/archive/master.zip
-	cd /data/aria2
-	unzip master.zip
-	mv yaaw-master/* ./
+#	echo "-------------------------------"
+#	read -p "设置用户名：" user
+#	read -p "设置密码：" pass
+#	read -p "设置Aria2授权令牌（字母或数字组合，不要包含特殊字符）：" rpc
+#	echo "-------------------------------"
+#	sed -i "s/rpc-secret=/rpc-secret=${rpc}/g" /data/aria2/aria2.conf
+#	#下载yaaw
+#	wget -P /data/aria2 https://github.com/binux/yaaw/archive/master.zip
+#	cd /data/aria2
+#	unzip master.zip
+#	mv yaaw-master/* ./
 	
-	#下载caddy server
-	wget http://soft.xiaoz.org/linux/caddy.filemanager -O caddy && mv caddy /usr/bin/
-	chmod +x /usr/bin/caddy
-	#修改配置
-	#sed -i "s/localhost/$1/g" /data/aria2/caddy.conf
-	sed -i "s/username/${user}/g" /data/aria2/caddy.conf
-	sed -i "s/password/${pass}/g" /data/aria2/caddy.conf
-	#放行端口
-	chk_firewall
-	#启动服务
-	cd /data/aria2
-	nohup aria2c --conf-path=/data/aria2/aria2.conf > /data/aria2/aria2.log 2>&1 &
-	nohup caddy -conf="/data/aria2/caddy.conf" > /data/aria2/caddy.log 2>&1 &
-	echo "-------------------------------"
-	echo "#####		安装完成，请牢记以下信息。	#####"
-	echo "访问地址：http://${osip}:6080"
-	echo "用户名：${user}"
-	echo "密码：${pass}"
-	echo "RPC地址：http://token:${rpc}@${osip}:6800/jsonrpc"
-	echo "-------------------------------"
-	echo "需要帮助请访问：https://www.xiaoz.me/archives/9694"
-	echo "-------------------------------"
-	#一点点清理工作
-	rm -rf /data/aria2/*.zip
-	rm -rf /data/aria2/*.tar.gz
-	rm -rf /data/aria2/*.txt
-	rm -rf /data/aria2/*.md
-	rm -rf /data/aria2/yaaw-*
-}
+#	#下载caddy server
+#	wget http://soft.xiaoz.org/linux/caddy.filemanager -O caddy && mv caddy /usr/bin/
+#	chmod +x /usr/bin/caddy
+#	#修改配置
+#	#sed -i "s/localhost/$1/g" /data/aria2/caddy.conf
+#	sed -i "s/username/${user}/g" /data/aria2/caddy.conf
+#	sed -i "s/password/${pass}/g" /data/aria2/caddy.conf
+#	#放行端口
+#	chk_firewall
+#	#启动服务
+#	cd /data/aria2
+#	nohup aria2c --conf-path=/data/aria2/aria2.conf > /data/aria2/aria2.log 2>&1 &
+#	nohup caddy -conf="/data/aria2/caddy.conf" > /data/aria2/caddy.log 2>&1 &
+#	echo "-------------------------------"
+#	echo "#####		安装完成，请牢记以下信息。	#####"
+#	echo "访问地址：http://${osip}:6080"
+#	echo "用户名：${user}"
+#	echo "密码：${pass}"
+#	echo "RPC地址：http://token:${rpc}@${osip}:6800/jsonrpc"
+#	echo "-------------------------------"
+#	echo "需要帮助请访问：https://www.xiaoz.me/archives/9694"
+#	echo "-------------------------------"
+#	#一点点清理工作
+#	rm -rf /data/aria2/*.zip
+#	rm -rf /data/aria2/*.tar.gz
+#	rm -rf /data/aria2/*.txt
+#	rm -rf /data/aria2/*.md
+#	rm -rf /data/aria2/yaaw-*
+#}
 
 #Aria2低版本设置
-function setting6(){
+function setting(){
 	mkdir -p /data/aria2
 	mkdir -p /data/aria2/download
 	touch /data/aria2/aria2.session
@@ -93,7 +93,7 @@ function setting6(){
 	sed -i "s/#rpc-user=/rpc-user=${user}/g" /data/aria2/aria2.conf
 	sed -i "s/#rpc-passwd=/rpc-passwd=${pass}/g" /data/aria2/aria2.conf
 	#下载yaaw
-	wget -P /data/aria2 https://github.com/binux/yaaw/archive/master.zip
+	wget -P /data/aria2 https://github.com/helloxz/yaaw/archive/master.zip
 	cd /data/aria2
 	unzip master.zip
 	mv yaaw-master/* ./
@@ -149,7 +149,7 @@ echo '----------------------------------'
 echo '请选择系统:'
 echo "1) CentOS 7 X64"
 echo "2) CentOS 6 X64"
-echo "3) Debian 8+ X64 or Ubuntu 16+ X64"
+echo "3) Debian 7+ X64 or Ubuntu 14+ X64"
 echo "q) 退出"
 echo '----------------------------------'
 read -p ":" num
@@ -166,7 +166,7 @@ case $num in
 	2)
 		#安装aria2
 		centos6
-		setting6 $osip
+		setting $osip
 		exit
 	;;
 	3)
